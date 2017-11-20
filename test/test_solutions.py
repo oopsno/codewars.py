1414236fe12a1fe9277a982f097c5b86bc028804
Yufei Liu <yufeiliu94@gmail.com>
2017-11-20 18:38:27 +0800

    :art: 改善单元测试与 PyCharm 的相容性

--- test/test_solutions.py
+++ test/test_solutions.py
@@ -7,9 +7,9 @@ import importlib
 import unittest
 
 
-def main():
-    runner = unittest.TextTestRunner()
-    root = os.path.abspath(os.path.relpath('..', start=os.path.dirname(__file__)))
+def for_each_case(handle: callable):
+    root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    sys.path.append(root)
     solutions = os.path.join(root, 'solutions')
     kyus = [item for item in os.listdir(solutions) if re.match(r'kyu\d', item)]
     for kyu in kyus:
@@ -20,11 +20,32 @@ def main():
             for key in dir(s):
                 cls = getattr(s, key)
                 if type(cls) is type and issubclass(cls, unittest.TestCase):
-                    show_name = '::'.join(s.capitalize() for s in name.split('.'))
-                    print('Running test: {}'.format(show_name), file=sys.stderr)
-                    suite = unittest.defaultTestLoader.loadTestsFromTestCase(cls)
-                    runner.run(suite)
+                    handle(kyu, cls)
+    from test.test_framework import TestFramework
+    handle('', TestFramework)
+
+
+class Run:
+    def __init__(self):
+        self.runner = unittest.TextTestRunner()
+
+    def __call__(self, kyu: str, cls: str):
+        show_name = '{}::{}'.format(kyu, cls.__name__)
+        print('Testing: {}'.format(show_name), file=sys.stderr)
+        suite = unittest.defaultTestLoader.loadTestsFromTestCase(cls)
+        self.runner.run(suite)
+
+
+class Inject:
+    def __init__(self, namespace: dict):
+        self.namespace = namespace
+
+    def __call__(self, kyu: str, cls: str):
+        # 强制 Unittest 逐 kyu 按字典非减序执行
+        self.namespace[kyu + cls.__name__] = cls
 
 
 if __name__ == '__main__':
-    main()
+    for_each_case(Run())
+else:
+    for_each_case(Inject(globals()))
