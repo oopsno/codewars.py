925f87d125eaac52ba941885a3080f48fd647999
Yufei Liu <yufeiliu94@gmail.com>
2017-11-29 22:02:18 +0800

    :art: 合并两个数独以降低理解复杂度

--- solutions/kyu5/did-i-finish-my-sudoku.py
+++ solutions/kyu5/did-i-finish-my-sudoku.py
@@ -38,7 +38,7 @@ def done_or_not(board):
 
 
 with codewars.Test(namespace=globals()) as test:
-    test.assert_equals(done_or_not([
+    sudoku = [
         [1, 3, 2, 5, 7, 9, 4, 6, 8],
         [4, 9, 8, 2, 6, 1, 3, 7, 5],
         [7, 5, 6, 3, 8, 4, 2, 1, 9],
@@ -47,15 +47,7 @@ with codewars.Test(namespace=globals()) as test:
         [9, 8, 7, 4, 2, 6, 5, 3, 1],
         [2, 1, 4, 9, 3, 5, 6, 8, 7],
         [3, 6, 5, 8, 1, 7, 9, 2, 4],
-        [8, 7, 9, 6, 4, 2, 1, 5, 3]]), 'Finished!')
-
-    test.assert_equals(done_or_not(
-        [[1, 3, 2, 5, 7, 9, 4, 6, 8],
-         [4, 9, 8, 2, 6, 1, 3, 7, 5],
-         [7, 5, 6, 3, 8, 4, 2, 1, 9],
-         [6, 4, 3, 1, 5, 8, 7, 9, 2],
-         [5, 2, 1, 7, 9, 3, 8, 4, 6],
-         [9, 8, 7, 4, 2, 6, 5, 3, 1],
-         [2, 1, 4, 9, 3, 5, 6, 8, 7],
-         [3, 6, 5, 8, 1, 7, 9, 2, 4],
-         [8, 7, 9, 6, 4, 2, 1, 3, 5], ]), 'Try again!')
+        [8, 7, 9, 6, 4, 2, 1, 5, 3]]
+    test.assert_equals(done_or_not(sudoku), 'Finished!')
+    sudoku[-1][-2], sudoku[-1][-1] = sudoku[-1][-1], sudoku[-1][-2]
+    test.assert_equals(done_or_not(sudoku), 'Try again!')
